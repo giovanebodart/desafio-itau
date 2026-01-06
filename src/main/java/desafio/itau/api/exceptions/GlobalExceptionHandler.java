94d548c603df2ff4b9da86e0196aa7361194229e
Giovane Bodart <giovanebodartperes7@gmail.com>
2026-01-06 14:18:42 -0300

    Atualizações.

--- src/main/java/desafio/itau/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/desafio/itau/api/exceptions/GlobalExceptionHandler.java
@@ -2,19 +2,16 @@ package desafio.itau.api.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ValorInvalido.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleValorInvalido(){
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).build();
     }
 
-    @ExceptionHandler(ValorNulo.class)
-    public ResponseEntity handleValorNulo(){
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).build();
-    }
 }
