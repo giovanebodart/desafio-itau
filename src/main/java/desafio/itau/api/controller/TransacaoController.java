46302ebd9e53e81b37f80696304d4a73db50b11d
Giovane Bodart <giovanebodartperes7@gmail.com>
2026-01-07 17:06:12 -0300

    .

--- src/main/java/desafio/itau/api/controller/TransacaoController.java
+++ src/main/java/desafio/itau/api/controller/TransacaoController.java
@@ -1,6 +1,7 @@
 package desafio.itau.api.controller;
 
 import desafio.itau.api.domain.Transacao;
+import desafio.itau.api.dto.EstatisticaDTO;
 import desafio.itau.api.dto.TransacaoDTO;
 import desafio.itau.api.service.TransacaoService;
 import jakarta.validation.Valid;
@@ -11,20 +12,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/transacao")
 public class TransacaoController {
 
     @Autowired
     private TransacaoService service;
 
-    @PostMapping("/transacao")
+    @PostMapping
     public ResponseEntity<Transacao> receberTransacao(@Valid @RequestBody TransacaoDTO dto){
         service.receber(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping("/transacao")
+    @DeleteMapping
     public ResponseEntity deletarTransacoes(){
         service.deletar();
         return ResponseEntity.status(HttpStatus.OK).build();
--- src/main/java/desafio/itau/api/service/TransacaoService.java
+++ src/main/java/desafio/itau/api/service/TransacaoService.java
@@ -1,9 +1,13 @@
 package desafio.itau.api.service;
 
 import desafio.itau.api.domain.Transacao;
+import desafio.itau.api.dto.EstatisticaDTO;
 import desafio.itau.api.dto.TransacaoDTO;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.DoubleSummaryStatistics;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -19,5 +23,13 @@ public class TransacaoService {
 
     public void deletar(){
         transacoes.clear();
+    }
+
+    public EstatisticaDTO visualizar(){
+        DoubleSummaryStatistics summaryStatistics = transacoes.stream()
+                .filter(t -> t.getDataHora().isAfter(OffsetDateTime.now().minusSeconds(60)))
+                .mapToDouble(Transacao::getValor)
+                .summaryStatistics();
+        return new EstatisticaDTO(summaryStatistics);
     }
 }
